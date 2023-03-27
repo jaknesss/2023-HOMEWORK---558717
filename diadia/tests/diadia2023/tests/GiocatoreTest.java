f1404a79b192bf8d865dc83e2dfe5e2851dfb74c
fra <fra.venditti3@stud.uniroma3.it>
2023-03-27 12:25:35 +0200

    rifatto il test di Giocatore

--- diadia/tests/diadia2023/tests/GiocatoreTest.java
+++ diadia/tests/diadia2023/tests/GiocatoreTest.java
@@ -1,6 +1,9 @@
 package diadia2023.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertEquals;
 
@@ -11,29 +14,69 @@ import it.uniroma3.diadia.giocatore.Borsa;
 import it.uniroma3.diadia.giocatore.Giocatore;
 
 class GiocatoreTest {
-
-	Giocatore giocatore = new Giocatore();
-	Borsa borsa = giocatore.getBorsa();	
+	
+	private final int MAX_CFU = 20;
+	private final int MAX_ATTREZZI = 10;
+	private final String ATTREZZO = "attrezzoTest";
+	private Giocatore giocatore;
+	private Borsa borsa;
+	
+	@BeforeEach
+	public void setUp() {
+		giocatore = new Giocatore();
+		borsa = giocatore.getBorsa();			
+	}
 	
 	@Test
 	public void testSetCfu() {
 		giocatore.setCfu(giocatore.getCfu()-2);
-		assertEquals(18, giocatore.getCfu());
+		assertEquals(MAX_CFU-2, giocatore.getCfu());
 	}
 	
 	@Test
-	public void testAddAttrezzo() {
-		Attrezzo attrezzo1 = new Attrezzo("test", 2);
+	public void testAddAttrezzoSingolo() {
+		Attrezzo attrezzo1 = new Attrezzo(ATTREZZO, 1);
 		giocatore.addAttrezzo(attrezzo1);
-		assertEquals(borsa.getAttrezzo("test"), attrezzo1);
+		assertEquals(attrezzo1, borsa.getAttrezzo(ATTREZZO));
 	}
 	
 	@Test
-	public void testRemoveAttrezzo() {
-		Attrezzo attrezzo1 = new Attrezzo("test", 2);
-		giocatore.addAttrezzo(attrezzo1);
-		giocatore.removeAttrezzo(attrezzo1);
-		assertFalse(borsa.hasAttrezzo(attrezzo1.getNome()));
+	public void testAddAttrezzoMaxCapacita() {
+		for(int i = 0; i < MAX_ATTREZZI; i++) {
+			assertTrue(giocatore.addAttrezzo(new Attrezzo(ATTREZZO, 1)));			
+		}
+		assertFalse(giocatore.addAttrezzo(new Attrezzo(ATTREZZO, 1)));
 	}
-
+	
+	@Test
+	public void testAddAttrezzoMaxPeso() {
+		for(int i = 0; i < MAX_ATTREZZI-1; i++) {
+			assertTrue(giocatore.addAttrezzo(new Attrezzo(ATTREZZO, 1)));			
+		}
+		assertFalse(giocatore.addAttrezzo(new Attrezzo(ATTREZZO, 2)));
+	}
+	
+	
+	@Test
+	public void testRemoveAttrezzoBorsaVuota() {
+		giocatore.removeAttrezzo(null);
+		assertFalse(borsa.hasAttrezzo(ATTREZZO));
+	}
+	
+	@Test
+	public void testRemoveAttrezzoSingolo() {
+		Attrezzo attrezzo = new Attrezzo(ATTREZZO, 1);
+		giocatore.addAttrezzo(attrezzo);
+		assertTrue(borsa.hasAttrezzo(ATTREZZO));
+		giocatore.removeAttrezzo(attrezzo);
+		assertFalse(borsa.hasAttrezzo(ATTREZZO));
+	}
+	
+	@Test
+	public void testRemoveAttrezzoInesistente() {
+		Attrezzo attrezzo = new Attrezzo(ATTREZZO, 1);
+		giocatore.removeAttrezzo(attrezzo);
+		assertFalse(borsa.hasAttrezzo(ATTREZZO));
+	}
+	
 }

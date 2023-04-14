f68e4b19f0a56248780ab3970254265b872299d4
fra <fra.venditti3@stud.uniroma3.it>
2023-04-14 11:04:43 +0200

    fixato test

--- diadia/tests/it/uniroma3/tests/ambienti/StanzaMagicaTest.java
+++ diadia/tests/it/uniroma3/tests/ambienti/StanzaMagicaTest.java
@@ -15,6 +15,7 @@ class StanzaMagicaTest {
 	private final int PESO_OGGETTO = 2;
 	private final String NOME_STANZA = "atrio";
 	private final String NOME_ATTR = "chiave";
+	private final String NOME_ATTR_INV = "evaihc";
 	private Stanza stanza;
 	private Attrezzo attrezzo;
 	
@@ -26,18 +27,41 @@ class StanzaMagicaTest {
 	}
 
 	@Test
-	void testAddAttrezzoSingoloPesoSogliaDefault() {
+	void testPeso_NonRaddoppiato() {
 		assertTrue(stanza.addAttrezzo(attrezzo));
 		assertEquals(PESO_OGGETTO, stanza.getAttrezzo(NOME_ATTR).getPeso() ); 
 	}
+
 	
 	@Test
-	void testAddAttrezziOltreSogliaDefault() {
+	void testPeso_Raddoppiato() {
 		StringBuilder nomeDaInvertire = new StringBuilder("nome");
 		Attrezzo attrDaTestare = new Attrezzo(nomeDaInvertire.toString(), PESO_OGGETTO);
-		for(int i = 0; i < SOGLIA_MAGICA-1; i++)
-			assertTrue(stanza.addAttrezzo(attrezzo));	
+		raggiungiSoglia();
 		stanza.addAttrezzo(attrDaTestare);
 		assertEquals(PESO_OGGETTO*2, stanza.getAttrezzo(nomeDaInvertire.reverse().toString()).getPeso()); 
 	}
+	
+	@Test
+	void testNome_NonInvertito() {
+		assertTrue(stanza.addAttrezzo(attrezzo));
+		assertEquals(NOME_ATTR, stanza.getAttrezzo(NOME_ATTR).getNome()); 
+	}
+	
+	@Test
+	void testNome_Invertito() {
+		StringBuilder nomeDaInvertire = new StringBuilder(NOME_ATTR);
+		Attrezzo attrDaTestare = new Attrezzo(nomeDaInvertire.toString(), PESO_OGGETTO);
+		raggiungiSoglia();
+		stanza.addAttrezzo(attrDaTestare);
+		assertEquals(NOME_ATTR_INV, stanza.getAttrezzo(nomeDaInvertire.reverse().toString()).getNome()); 
+	}
+	
+	
+	
+	void raggiungiSoglia() {
+		for(int i = 0; i < SOGLIA_MAGICA-1; i++)
+			assertTrue(stanza.addAttrezzo(attrezzo));	
+	}
+	
 }
